a90415b8c266a51477638cd14e9f15c9adabb492
hsforschool <167404440+hsforschool@users.noreply.github.com>
2025-01-13 10:45:26 +0800

    Add files via upload
    added subclassed :DDDDDDDD

---
+++ regenhector_prg2_assignment/regenhector_prg2_assignment/NORMFlight.cs
@@ -0,0 +1,20 @@
+﻿namespace regenhector_prg2_assignment;
+
+public class NORMFlight : Flight
+{
+    //constructors
+    public NORMFlight() {}
+    public NORMFlight(string fn, string o, string d, DateTime et, string s) : base(fn, o, d, et, s) {}
+
+    //methods
+    public override double CalculateFees()
+    {
+        //pls fix
+        return 0;
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
