2dca538c177c6a87b3a6d599ae88b17ac2d69c87
hsforschool <167404440+hsforschool@users.noreply.github.com>
2025-01-14 00:23:05 +0800

    added feature 1

--- regenhector_prg2_assignment/regenhector_prg2_assignment/Program.cs
+++ regenhector_prg2_assignment/regenhector_prg2_assignment/Program.cs
@@ -2,6 +2,33 @@
 {
     class Program
     {
+        static void InitAirlines(Terminal terminal)
+        {
+            using (StreamReader sr = new StreamReader("airlines.csv"))
+            {
+                string? s = sr.ReadLine();
+                while ((s = sr.ReadLine()) != null)
+                {
+                    string[] airlineInfo = s.Split(",");
+                    terminal.AddAirline(new Airline(airlineInfo[0], airlineInfo[1]));
+                }
+            }
+        }
+
+        static void InitBoardingGates(Terminal terminal)
+        {
+            using (StreamReader sr = new StreamReader("boardinggates.csv"))
+            {
+                string? s = sr.ReadLine();
+                while ((s = sr.ReadLine()) != null)
+                {
+                    string[] boardingGateInfo = s.Split(",");
+                    terminal.AddBoardingGate(new BoardingGate(boardingGateInfo[0],
+                        Convert.ToBoolean(boardingGateInfo[1]), Convert.ToBoolean(boardingGateInfo[2]),
+                        Convert.ToBoolean(boardingGateInfo[3])));
+                }
+            }
+        }
         static void Main()
         {
             
