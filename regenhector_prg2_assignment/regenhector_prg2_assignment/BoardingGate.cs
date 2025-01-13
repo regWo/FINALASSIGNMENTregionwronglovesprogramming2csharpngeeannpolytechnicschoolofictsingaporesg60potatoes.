e8a5fbeced7ab53d0d0da99914259396815826c9
regenwongle@gmail.com <167403832+regWo@users.noreply.github.com>
2025-01-14 00:35:26 +0800

    boardinggate.cs constructor/method fixes

--- regenhector_prg2_assignment/regenhector_prg2_assignment/BoardingGate.cs
+++ regenhector_prg2_assignment/regenhector_prg2_assignment/BoardingGate.cs
@@ -1,34 +1,45 @@
-﻿namespace regenhector_prg2_assignment;
-
-public class BoardingGate
+﻿namespace regenhector_prg2_assignment
 {
-    //attributes
-    public string GateName { get; set; }
-    public bool SupportsCFFT { get; set; }
-    public bool SupportsDDJB { get; set; }
-    public bool SupportsLWTT { get; set; }
-    public Flight Flight { get; set; }
-
-    //constructors
-    public BoardingGate() {}
-
-    public BoardingGate(string gn, bool cfft, bool ddjb, bool lwtt, Flight f)
+    public class BoardingGate
     {
-        GateName = gn;
-        SupportsCFFT = cfft;
-        SupportsDDJB = ddjb;
-        SupportsLWTT = lwtt;
-        Flight = f;
-    }
+        //attributes
+        public string GateName { get; set; }
+        public bool SupportsCFFT { get; set; }
+        public bool SupportsDDJB { get; set; }
+        public bool SupportsLWTT { get; set; }
+        //ok i think they want us to reassign the boarding gate's flight so terminalFive.BoardingGates[GateName] = new Flight(...);
+        public Flight Flight { get; set; }
 
-    //methods
-    public double CalculateFees()
-    {
-        return 300;
-    }
+        //constructors
+        public BoardingGate() { }
 
-    public override string ToString()
-    {
-        return $"{GateName} {SupportsCFFT} {SupportsDDJB} {SupportsLWTT} {Flight}";
+        public BoardingGate(string gn, bool cfft, bool ddjb, bool lwtt)
+        {
+            GateName = gn;
+            SupportsCFFT = cfft;
+            SupportsDDJB = ddjb;
+            SupportsLWTT = lwtt;
+        }
+
+        public BoardingGate(string gn, bool cfft, bool ddjb, bool lwtt, Flight f)
+        {
+            GateName = gn;
+            SupportsCFFT = cfft;
+            SupportsDDJB = ddjb;
+            SupportsLWTT = lwtt;
+            Flight = f;
+        }
+
+        //methods
+        public double CalculateFees()
+        {
+            const double baseBoardingGateFee = 300;
+            return baseBoardingGateFee;
+        }
+
+        public override string ToString()
+        {
+            return $"{GateName} {SupportsCFFT} {SupportsDDJB} {SupportsLWTT} {Flight}";
+        }
     }
 }
