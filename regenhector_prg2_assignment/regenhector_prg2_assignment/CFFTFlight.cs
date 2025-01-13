471d2c9aab2b625102cfef235bb035834f8317ee
regenwongle@gmail.com <167403832+regWo@users.noreply.github.com>
2025-01-14 00:32:49 +0800

    added constructor

--- regenhector_prg2_assignment/regenhector_prg2_assignment/CFFTFlight.cs
+++ regenhector_prg2_assignment/regenhector_prg2_assignment/CFFTFlight.cs
@@ -1,26 +1,30 @@
-﻿namespace regenhector_prg2_assignment;
-
-public class CFFTFlight : Flight
+﻿namespace regenhector_prg2_assignment
 {
-    //attributes
-    public double RequestFee { get; set; }
-
-    //constructors
-    public CFFTFlight() {}
-
-    public CFFTFlight(string fn, string o, string d, DateTime et, string s, double rf) : base(fn, o, d, et, s)
+    public class CFFTFlight : Flight
     {
-        RequestFee = rf;
-    }
+        //attributes
+        public double RequestFee { get; set; } = 150;
 
-    //methods
-    public override double CalculateFees()
-    {
-        return base.CalculateFees() + 150;
-    }
+        //constructors
+        public CFFTFlight() { }
 
-    public override string ToString()
-    {
-        return base.ToString() + $" {RequestFee}";
+        public CFFTFlight(string fn, string o, string d, DateTime et) : base(fn, o, d, et) { }
+
+        //i have no clue if we need this since RequestFee should be a constant value and always use the default value when class is instantiated
+        public CFFTFlight(string fn, string o, string d, DateTime et, string s, double rf) : base(fn, o, d, et, s)
+        {
+            RequestFee = rf;
+        }
+
+        //methods
+        public override double CalculateFees()
+        {
+            return base.CalculateFees() + 150;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $" {RequestFee}";
+        }
     }
 }
