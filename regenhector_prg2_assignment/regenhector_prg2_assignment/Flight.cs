b6e7168a1d1744fb24e465a58ebbca2715ab39c8
regenwongle@gmail.com <167403832+regWo@users.noreply.github.com>
2025-01-14 00:31:38 +0800

    added constructor

--- regenhector_prg2_assignment/regenhector_prg2_assignment/Flight.cs
+++ regenhector_prg2_assignment/regenhector_prg2_assignment/Flight.cs
@@ -1,38 +1,47 @@
-﻿namespace regenhector_prg2_assignment;
-
-public abstract class Flight
+﻿namespace regenhector_prg2_assignment
 {
-    //attributes
-    public string FlightNumber { get; set; }
-    public string Origin { get; set; }
-    public string Destination { get; set; }
-    public DateTime ExpectedTime { get; set; }
-    public string Status { get; set; }
-
-    //constructors
-    public Flight() {}
-
-    public Flight(string fn, string o, string d, DateTime et, string s)
+    public abstract class Flight
     {
-        FlightNumber = fn;
-        Origin = o;
-        Destination = d;
-        ExpectedTime = et;
-        Status = s;
-    }
+        //attributes
+        public string FlightNumber { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public DateTime ExpectedTime { get; set; }
+        public string Status { get; set; }
 
-    //methods
-    public virtual double CalculateFees()
-    {
-        //if both values > 0 something has gone very wrong; either value should be > 0, not both
-        double singaporeOriginFee = (Origin == "Singapore (SIN)") ? 800 : 0;
-        double singaporeArrivingFee = (Destination == "Singapore (SIN)") ? 500 : 0;
+        //constructors
+        public Flight() { }
 
-        return singaporeOriginFee + singaporeArrivingFee;
-    }
+        public Flight(string fn, string o, string d, DateTime et)
+        {
+            FlightNumber = fn;
+            Origin = o;
+            Destination = d;
+            ExpectedTime = et;
+        }
 
-    public override string ToString()
-    {
-        return $"{FlightNumber} {Origin} {Destination} {ExpectedTime.ToString("yyyy/MM/dd")} {Status}";
+        public Flight(string fn, string o, string d, DateTime et, string s)
+        {
+            FlightNumber = fn;
+            Origin = o;
+            Destination = d;
+            ExpectedTime = et;
+            Status = s;
+        }
+
+        //methods
+        public virtual double CalculateFees()
+        {
+            //if both values > 0 something has gone very wrong; either value should be > 0, not both
+            double singaporeOriginFee = (Origin == "Singapore (SIN)") ? 800 : 0;
+            double singaporeArrivingFee = (Destination == "Singapore (SIN)") ? 500 : 0;
+
+            return singaporeOriginFee + singaporeArrivingFee;
+        }
+
+        public override string ToString()
+        {
+            return $"{FlightNumber} {Origin} {Destination} {ExpectedTime.ToString("dd/MM/yyyy h:MM:ss tt")} {Status}";
+        }
     }
 }
