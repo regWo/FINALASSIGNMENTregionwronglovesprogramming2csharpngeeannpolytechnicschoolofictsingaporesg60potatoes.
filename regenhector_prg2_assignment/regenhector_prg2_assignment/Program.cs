9adb2ea8bc745cd3f4f5e6e88b7cac436757ec64
regenwongle@gmail.com <167403832+regWo@users.noreply.github.com>
2025-01-26 16:10:51 +0800

    added feature 2

--- regenhector_prg2_assignment/regenhector_prg2_assignment/Program.cs
+++ regenhector_prg2_assignment/regenhector_prg2_assignment/Program.cs
@@ -2,8 +2,63 @@
 {
     class Program
     {
+        static void InitBoardingGates(Terminal terminal)
+        {
+            using (StreamReader sr = new StreamReader("boardinggates.csv"))
+            {
+                string? s = sr.ReadLine();
+                while ((s = sr.ReadLine()) != null)
+                {
+                    string[] boardingGateInfo = s.Split(",");
+                    terminal.AddBoardingGate(new BoardingGate(boardingGateInfo[0],
+                        Convert.ToBoolean(boardingGateInfo[1]), Convert.ToBoolean(boardingGateInfo[2]),
+                        Convert.ToBoolean(boardingGateInfo[3])));
+                }
+            }
+        }
+
+        static string DisplayMainMenu()
+        {
+            Console.WriteLine("=============================================");
+            Console.WriteLine("Welcome to Changi Airport Terminal 5");
+            Console.WriteLine("=============================================");
+            Console.WriteLine("1. List All Flights");
+            Console.WriteLine("2. List Boarding Gates");
+            Console.WriteLine("3. Assign a Boarding Gate to a Flight");
+            Console.WriteLine("4. Create Flight");
+            Console.WriteLine("5. Display Airline Flights");
+            Console.WriteLine("6. Modify Flight Details");
+            Console.WriteLine("7. Display Flight Schedule");
+            Console.WriteLine("0. Exit");
+            Console.Write("Please select your option: ");
+            string userOption = Console.ReadLine();
+
+            return userOption;
+        }
+
         static void Main()
         {
+            //DO NOT TOUCH THESE ARE THE INITIALISERS
+            Terminal terminalFive = new Terminal("Terminal Name");
+            InitBoardingGates(terminalFive); 
+            //DO NOT TOUCH THESE ARE THE INITIALISERS
+
+            while (true)
+            {
+                string userOption = DisplayMainMenu();
+                if (userOption == "0") break;
+
+                switch (userOption)
+                {
+                    case "1":
+                        
+                        break;
+                    case "2":
+
+                        break;
+                }
+            }
+
             
         }
     }
