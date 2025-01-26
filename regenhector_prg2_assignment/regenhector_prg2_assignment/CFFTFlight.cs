6880cc1351e7e88f316cf428605defe8ab03b676
hsforschool <167404440+hsforschool@users.noreply.github.com>
2025-01-26 16:59:15 +0800

    CFFTFlight changed value to requestfee

--- regenhector_prg2_assignment/regenhector_prg2_assignment/CFFTFlight.cs
+++ regenhector_prg2_assignment/regenhector_prg2_assignment/CFFTFlight.cs
@@ -19,7 +19,7 @@
         //methods
         public override double CalculateFees()
         {
-            return base.CalculateFees() + 150;
+            return base.CalculateFees() + RequestFee;
         }
 
         public override string ToString()
