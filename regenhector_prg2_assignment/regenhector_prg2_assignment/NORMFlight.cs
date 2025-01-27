e50491589420f1cf94088ef09010423ab5075b8a
regWo <s10267461@connect.np.edu.sg>
2025-01-27 10:30:09 +0800

    removed constructor

--- regenhector_prg2_assignment/regenhector_prg2_assignment/NORMFlight.cs
+++ regenhector_prg2_assignment/regenhector_prg2_assignment/NORMFlight.cs
@@ -5,8 +5,6 @@
         //constructors
         public NORMFlight() { }
 
-        public NORMFlight(string fn, string o, string d, DateTime et) : base(fn, o, d, et) { }
-
         //i have no clue if we need this since RequestFee should be a constant value and always use the default value when class is instantiated
         public NORMFlight(string fn, string o, string d, DateTime et, string s) : base(fn, o, d, et, s) { }
 
